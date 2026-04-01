7b9c9186f4dfda6345b53cc23b346a4497d6e9f9
teachain <281477138@qq.com>
2026-04-01 17:27:27 +0800

    fix: use strconv.Itoa for port conversion instead of string(rune(...))

---
+++ services/accesshttp/internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"strconv"
+)
+
+type Config struct {
+	Server  ServerConfig  `mapstructure:"server"`
+	Workers int           `mapstructure:"workers"`
+	Backend BackendConfig `mapstructure:"backend"`
+}
+
+type ServerConfig struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+func (s ServerConfig) Addr() string {
+	return s.Host + ":" + strconv.Itoa(s.Port)
+}
+
+type BackendConfig struct {
+	MatchEngine string `mapstructure:"matchengine"`
+	MarketPrice string `mapstructure:"marketprice"`
+	ReadHistory string `mapstructure:"readhistory"`
+}
+
+func Load(path string) (*Config, error) {
+	viper.SetConfigFile(path)
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
